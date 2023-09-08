53aebb2cb7fbdb51480ea89bc0f325cb210c52e3
TanyaGupta1901 <tanyag1901@gmail.com>
2023-09-08 14:40:04 +0530

    grammar fix

--- pages/index.js
+++ pages/index.js
@@ -70,7 +70,7 @@ const Home = () => {
     animate={{translateX:0}}
     transition={{duration: 1, ease: "easeIn"}}
     className="absolute bottom-25 right-20 md:bottom-10 md:right-10 border-accent border p-2 rounded-full hidden md:block">
-      <p className="text-[12px] text-accent">Try hovering on the doll!</p>
+      <p className="text-[12px] text-accent">Try hovering on the figurine!</p>
     </motion.div>
     </>
   );
